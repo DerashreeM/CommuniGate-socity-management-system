e557cee887e22383564c669164581d240891523b
Meet Mehta <148692742+DerashreeM@users.noreply.github.com>
2024-04-07 20:06:21 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+ $conn = mysqli_connect('localhost','root','','communigate');
+
+?>
+
+<!-- //
+
+// $conn = mysqli_connect('localhost','id21189497_root','1@Communigate','id21189497_communigate'); -->
